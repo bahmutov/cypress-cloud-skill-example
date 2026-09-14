48b0a909b067c99352ac998e5e1d73caf1e65a4f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2026-09-14 09:45:32 -0400

    make the second test fail

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,6 +2,9 @@
 
 describe('Suite A', () => {
   it('works 1', () => {})
-  it('works 2', () => {})
+  it('works 2', () => {
+    // make this test fail
+    throw new Error('This test is supposed to fail')
+  })
   it('works 3', () => {})
 })
